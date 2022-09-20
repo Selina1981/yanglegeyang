cb5aec1ae321eab4cad21c5d43d56611cfbdbb23
Shipin <shpzhao@rxhui.com>
2022-09-20 09:35:16 +0800

    多玩家

--- src/main/java/sheep.java
+++ src/main/java/sheep.java
@@ -4,7 +4,7 @@ public class sheep {
 	public static void main(String[] args) {
 		Player 李斌 = new Player("李斌", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQ0Mjg0MTUsIm5iZiI6MTY2MzMyNjIxNSwiaWF0IjoxNjYzMzI0NDE1LCJqdGkiOiJDTTpjYXRfbWF0Y2g6bHQxMjM0NTYiLCJvcGVuX2lkIjoiIiwidWlkIjoyNDExMDMyNywiZGVidWciOiIiLCJsYW5nIjoiIn0.Miz8rpl4YRWG63TRRgMBIAx56vG8mfU3nkYS5_Mz5tM");
 		李斌.play();
-		Player 赵士品 = new Player("我", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQyNTUyNTUsIm5iZiI6MTY2MzE1MzA1NSwiaWF0IjoxNjYzMTUxMjU1LCJqdGkiOiJDTTpjYXRfbWF0Y2g6bHQxMjM0NTYiLCJvcGVuX2lkIjoiIiwidWlkIjozMjI0Mjc1OSwiZGVidWciOiIiLCJsYW5nIjoiIn0.OIOFRB_UiFrnL38okzusU6LlMqrFhEA-DJ9PpKxpG6M");
+		Player 赵士品 = new Player("赵士品", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQyNTUyNTUsIm5iZiI6MTY2MzE1MzA1NSwiaWF0IjoxNjYzMTUxMjU1LCJqdGkiOiJDTTpjYXRfbWF0Y2g6bHQxMjM0NTYiLCJvcGVuX2lkIjoiIiwidWlkIjozMjI0Mjc1OSwiZGVidWciOiIiLCJsYW5nIjoiIn0.OIOFRB_UiFrnL38okzusU6LlMqrFhEA-DJ9PpKxpG6M");
 		赵士品.play();
 
 	}
